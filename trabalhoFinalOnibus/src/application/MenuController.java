cd32bb9a7b39ec917669c65d9e52ebd5f497f740
Josiel <josielbueno35@gmail.com>
2018-09-05 20:36:58 -0300

    Fxml.Menu e Fxml.CadastroEmpressa

--- trabalhoFinalOnibus/src/application/MenuController.java
+++ trabalhoFinalOnibus/src/application/MenuController.java
@@ -1,5 +1,65 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
 public class MenuController {
-	
+
+    @FXML
+    private MenuItem mmiEmpresa;
+
+    @FXML
+    private MenuItem mmiDespesas;
+
+    @FXML
+    private MenuItem mmiManutencao;
+
+    @FXML
+    private MenuItem mmiControleFinanceiro;
+
+    @FXML
+    private MenuItem mmiVeiculo;
+
+    @FXML
+    private MenuItem mmiFuncionario;
+
+    @FXML
+    private BorderPane bpPrincipal;
+
+    @FXML
+    void MenuEmpresa(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void MenuVeiculo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuFuncionario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuManutencao(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuControleFinanceiro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuDespesas(ActionEvent event) {
+
+    }
+
 }
