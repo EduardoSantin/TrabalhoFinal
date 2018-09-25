740dd8fcbdc755fa84914a3e9510cbaf43fd9033
Lucas Berlanda <lucasberlanda02@gmail.com>
2018-09-24 21:10:28 -0300

    Botão menu para Login

--- trabalhoFinalOnibus/src/application/MenuController.java
+++ trabalhoFinalOnibus/src/application/MenuController.java
@@ -5,36 +5,40 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class MenuController {
 
-    @FXML
-    private MenuItem mmiEmpresa;
+	@FXML
+	private MenuItem mmiEmpresa;
 
-    @FXML
-    private MenuItem mmiDespesas;
+	@FXML
+	private MenuItem mmiDespesas;
 
-    @FXML
-    private MenuItem mmiManutencao;
+	@FXML
+	private MenuItem mmiManutencao;
 
-    @FXML
-    private MenuItem mmiControleFinanceiro;
+	@FXML
+	private MenuItem mmiControleFinanceiro;
 
-    @FXML
-    private MenuItem mmiVeiculo;
+	@FXML
+	private MenuItem mmiVeiculo;
 
-    @FXML
-    private MenuItem mmiFuncionario;
+	@FXML
+	private MenuItem mmiFuncionario;
 
-    @FXML
-    private BorderPane bpPrincipal;
+	@FXML
+	private BorderPane bpPrincipal;
 
-    @FXML
-    void MenuEmpresa(ActionEvent event) {
-    	FXMLLoader loader = new FXMLLoader();
+	@FXML
+	private Button btnEntrar;
+
+	@FXML
+	void MenuEmpresa(ActionEvent event) {
+		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("CadastroEmpresa.fxml"));
 		try {
 			AnchorPane menuView = (AnchorPane) loader.load();
@@ -42,39 +46,45 @@ public class MenuController {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-    }
+	}
 
-    @FXML
-    void MenuVeiculo(ActionEvent event) {
-    	FXMLLoader loader = new FXMLLoader();
-    	loader.setLocation(getClass().getResource("Veiculo.fxml"));
-    	try {
-    		AnchorPane menuView = (AnchorPane) loader.load();
-    		bpPrincipal.setCenter(menuView);
-    	} catch (IOException e1) {
-    		e1.printStackTrace();
-    	}
+	@FXML
+	void MenuVeiculo(ActionEvent event) {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("Veiculo.fxml"));
+		try {
+			AnchorPane menuView = (AnchorPane) loader.load();
+			bpPrincipal.setCenter(menuView);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 
-    }
+	}
 
-    @FXML
-    void MenuFuncionario(ActionEvent event) {
+	@FXML
+	void MenuFuncionario(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void MenuManutencao(ActionEvent event) {
+	@FXML
+	void MenuManutencao(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void MenuControleFinanceiro(ActionEvent event) {
+	@FXML
+	void MenuControleFinanceiro(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void MenuDespesas(ActionEvent event) {
+	@FXML
+	void MenuDespesas(ActionEvent event) {
 
-    }
+	}
+	
+
+	@FXML
+	void entrar(ActionEvent event) {
+		// fazer dialog de login
+	}
 
 }
