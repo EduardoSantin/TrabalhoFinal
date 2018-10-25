96794876ce9c0e26783bbd87b316ac6908fda475
Lucas Berlanda <lucasberlanda02@gmail.com>
2018-10-24 23:02:39 -0200

    Feito controles de movimentos com jdbc model e dao

---
+++ trabalhoFinalOnibus/src/principal/dao/MovimentosDAO.java
@@ -0,0 +1,7 @@
+package principal.dao;
+
+import principal.model.Movimentos;
+
+public interface MovimentosDAO extends CrudDAO<Movimentos>{
+
+}
---
+++ trabalhoFinalOnibus/src/principal/model/Movimentos.java
@@ -0,0 +1,32 @@
+package principal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="codigo")
+@ToString
+public class Movimentos {
+	
+	private Integer codigo;
+	private Integer numeroNota;
+	private String dtaEmissao;
+	private Double kmInicial;
+	private Double kmFinal;
+	private Integer qtdPassageiros;
+	private String origem;
+	private String destino;
+	private String motorista;
+	private Veiculo veiculo;
+	
+
+
+
+}
