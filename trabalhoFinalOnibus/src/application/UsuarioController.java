7218dcdc1dd95ad39415c9f626619ae5307cff91
Lucas Berlanda <lucasberlanda02@gmail.com>
2018-11-19 20:46:59 -0200

    feito botao voltar do cadastro usuario

--- trabalhoFinalOnibus/src/application/UsuarioController.java
+++ trabalhoFinalOnibus/src/application/UsuarioController.java
@@ -1,9 +1,14 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import principal.dao.UsuarioDAO;
 import principal.dao.UsuarioJDBC;
 import principal.model.Usuario;
@@ -49,5 +54,27 @@ public class UsuarioController {
 		tfsenha.clear();
 		usuario = new Usuario();
 	}
+	@FXML
+	private Button btnVoltarMenu;
+	
+	@FXML
+	void Voltar(ActionEvent event) {
+		btnVoltarMenu.setOnAction(new EventHandler<ActionEvent>() {
 
+			@Override
+			public void handle(ActionEvent event) {
+				Stage stage = new Stage();
+				Parent root = null;
+				try {
+					root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				Scene scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+				btnVoltarMenu.getScene().getWindow().hide();
+			}
+		});
+	}
 }
