14ff630071cf609e9d452c3ec5296aaac49b71e9
Lucas Berlanda <lucasberlanda02@gmail.com>
2018-11-29 21:49:10 -0200

    testVeiculo iniciado

---
+++ trabalhoFinalOnibus/src/test/VeiculoTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.junit.Test;
+
+import principal.dao.VeiculoDAO;
+import principal.dao.VeiculoJDBC;
+import principal.model.Veiculo;
+
+public class VeiculoTest {
+
+	private void assertArrayEquals(int i, Integer codigo) {
+	}
+	private void assertStringEquals(String i, String j) {}
+	
+	@Test
+	public void testaInsertVeiculo() throws Exception{
+		VeiculoDAO veiculodao = new VeiculoJDBC();
+		
+		Veiculo v = new Veiculo();
+		v.setRenavan(1234);
+		v.setMarca("scania");
+		v.setModelo("bomba");
+		v.setPlaca("MMM_1313");
+		v.setChassi(856562);
+		v.setMotor("escania");
+		
+		veiculodao.inserir(v);
+		
+		veiculodao.listar();
+		assertArrayEquals(1234, v.getRenavan());
+		assertStringEquals("scania", v.getMarca());
+		assertStringEquals("bomba", v.getModelo());
+		assertStringEquals("bomba", v.getModelo());
+		assertStringEquals("MMM-1313", v.getPlaca());
+		assertArrayEquals(856562, v.getChassi());
+		assertStringEquals("escania", v.getMotor());
+	}
+	
+	
+}
+
